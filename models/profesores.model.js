07cfda8b6e9501818fb0165f892fe829e63a8760
daniel M <daniel.marin.depablo@gmail.com>
2021-06-03 18:05:03 +0200

    created method profesores

--- models/profesores.model.js
+++ models/profesores.model.js
@@ -9,4 +9,22 @@ const getAllProfesores = (limit = 10, page = 1) => {
     })
 }
 
-module.exports = { getAllProfesores, }
+const createProfesor = ({ experiencia, precio, material_propio, niveles, desplazamiento, rango_desplazamiento, nombre, apellido, email, direccion, telefono }) => {
+    return new Promise((resolve, reject) => {
+        db.query('INSERT * FROM profesores INSERT INTO profesores (experiencia, precio, material_propio, niveles, desplazamiento, rango_desplazamiento, nombre, apellido, email, direccion, telefono) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [experiencia, precio, material_propio, niveles, desplazamiento, rango_desplazamiento, nombre, apellido, email, direccion, telefono], (err, results) => {
+            if (err) reject(err);
+            resolve(results);
+        })
+    })
+}
+
+const filterByPrice = (price1 = 1, price2 = 50) => {
+    return new Promise((resolve, reject) => {
+        db.query('SELECT * FROM profesores where precio >= ? and precio <= ? order by precio asc', [price1, price2], (err, rows) => {
+            if (err) reject(err);
+            resolve(rows)
+        })
+    })
+}
+
+module.exports = { getAllProfesores, filterByPrice, createProfesor }
--- routes/api/profesores.js
+++ routes/api/profesores.js
@@ -1,10 +1,28 @@
 const router = require('express').Router();
-const { getAllProfesores } = require('../../models/profesores.model')
+const { getAllProfesores, filterByPrice, createProfesor } = require('../../models/profesores.model')
 
 router.get('/', async (req, res) => {
     try {
         const profesores = await getAllProfesores();
         res.json(profesores)
+    } catch (err) {
+        res.json({ error: err.message })
+    }
+})
+
+router.get('/price', async (req, res) => {
+    try {
+        const prices = await filterByPrice();
+        res.json(prices)
+    } catch (err) {
+        res.json({ error: err.message })
+    }
+})
+
+router.post('/create', async (req, res) => {
+    try {
+        const createProf = await createProfesor(req.body);
+        res.json(createProf)
     } catch (err) {
         res.json({ error: err.message })
     }
