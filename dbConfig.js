561462c16c80fc62ceb0c840cab6426aec39bfec
Víctor Rascón <vrascon.sedano89@gmail.com>
2021-06-17 09:29:29 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- dbConfig.js
+++ dbConfig.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
+<<<<<<< Updated upstream
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT
+=======
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    database: 'wepadel_bbdd',
+    port: 8889
+>>>>>>> Stashed changes
 });
 
 global.db = pool;
