81a6b52e0d64fc18bdb21188941b7f700194aa6b
daniel M <daniel.marin.depablo@gmail.com>
2021-06-18 10:15:22 +0200

    add getallprofesor

--- models/profesores.model.js
+++ models/profesores.model.js
@@ -2,7 +2,7 @@
 
 const getAllProfesores = (limit = 10, page = 1) => {
     return new Promise((resolve, reject) => {
-        db.query('SELECT * FROM profesores LIMIT ?,?', [limit * (page - 1), limit], (err, rows) => {
+        db.query('SELECT usuarios.*,profesores.* FROM usuarios, profesores WHERE usuarios.id = profesores.fk_usuario LIMIT ?,?', [limit * (page - 1), limit], (err, rows) => {
             if (err) reject(err);
             resolve(rows)
         })
