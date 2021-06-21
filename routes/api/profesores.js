fe6c9c5ba3c6c4a29c859bd4415b1368ab345007
daniel M <daniel.marin.depablo@gmail.com>
2021-06-21 17:11:11 +0200

    method add profesor

--- routes/api/profesores.js
+++ routes/api/profesores.js
@@ -48,7 +48,8 @@ router.get('/instalations/:boolean', async (req, res) => {
 
 router.post('/create', async (req, res) => {
     try {
-        const createProf = await createProfesor(req.body);
+        req.user.id = req.body.fk_usuario
+        const createProf = await createProfesor(req.body, req.user.id);
         res.json(createProf)
     } catch (err) {
         res.json({ error: err.message })
